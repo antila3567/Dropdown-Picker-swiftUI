889d4b161a25ebd5627e10614d730822e28ccf2e
IvanL <antila3567@gmail.com>
2023-12-27 14:05:31 +0200

    Initial Commit

---
+++ DropdownPicker/DropdownPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropdownPickerApp.swift
+//  DropdownPicker
+//
+//  Created by Иван Легенький on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DropdownPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
