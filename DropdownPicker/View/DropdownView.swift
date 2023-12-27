cc3fdc181a7e46e4dd16b00e55bee8333ce5b5e5
IvanL <antila3567@gmail.com>
2023-12-27 14:52:18 +0200

    initial commit

---
+++ DropdownPicker/View/DropdownView.swift
@@ -0,0 +1,18 @@
+//
+//  DropdownView.swift
+//  DropdownPicker
+//
+//  Created by Иван Легенький on 27.12.2023.
+//
+
+import SwiftUI
+
+struct DropdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropdownView()
+}
